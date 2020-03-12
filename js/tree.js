f020b72be71b0ce4b56228d50e8c7b16f5484fbb
Nick O'Leary <nick.oleary@gmail.com>
2020-03-12 00:40:15 +0000

    Optimise branches and add stats counts

--- js/tree.js
+++ js/tree.js
@@ -2,9 +2,13 @@
 // http://algorithmicbotany.org/papers/colonization.egwnp2007.large.pdf
 
 
+
 var gui = new dat.GUI({
     height : 5 * 32 - 1
 });
+
+var statsGui = new dat.GUI({height : 5 * 32 - 1,autoPlace: true});
+statsGui.domElement.id = "report"
 
 var params = {
     trunkBaseRadius: 0.2,
@@ -15,13 +19,16 @@ var params = {
     branchSteps: 6,
     branchStepSize: 0.3,
     branchRadialSegments: 5,
-    drawBranchPoints: false,
+    branchSimplifyEpsilon: 0.2,
+        drawBranchPoints: true,
+    drawBranchFullPath: false,
+    attractionPointArc: Math.PI/2,
     attractionPointRadius: 5,
     attractionPointCount: 50,
     attractionPointHeightOffset: -1,
     drawAttractionPoints: false,
     leafDepthRatio: 0.2,
-    drawFoliage: true,
+        drawFoliage: false,
     drawTrunk: true,
     attractionVerticalRatio: 1
 };
@@ -57,8 +64,9 @@ branchFolder.add(params,"branchRadius").name("Radius").min(0.01).max(0.3).step(0
 branchFolder.add(params,"branchRadialSegments").name("Radial Segs").min(3).max(9).step(1);
 branchFolder.add(params,"branchSteps").name("Steps").min(1).max(20).step(1);
 branchFolder.add(params,"branchStepSize").name("Step Size").min(0.1).max(1).step(0.1);
+branchFolder.add(params,"branchSimplifyEpsilon").name("Simplify factor").min(0.005).max(0.8).step(0.005);
 branchFolder.add(params,"drawBranchPoints").name("Draw Points");
-
+branchFolder.add(params,"drawBranchFullPath").name("Draw Full Path");
 var leafFolder = gui.addFolder('Foliage');
 leafFolder.open();
 leafFolder.add(params,"drawFoliage").name("Draw");
@@ -70,8 +78,17 @@ apFolder.add(params,"attractionPointCount").name("Count").min(1).max(500).step(1
 apFolder.add(params,"attractionPointRadius").name("Radius").min(0.1).max(10).step(0.1);
 apFolder.add(params,"attractionPointHeightOffset").name("Height Offset").min(-2).max(2).step(0.2);
 apFolder.add(params,"attractionVerticalRatio").name("Vertical Ratio").min(0.1).max(5).step(0.1);
+apFolder.add(params,"attractionPointArc").name("Elevation Arc").min(0.1).max(3.1).step(0.1);
 
 apFolder.open();
+
+var treeStats = {
+    vertexCount:"",
+    branchPointsRemoved:""
+}
+statsGui.add(treeStats,"vertexCount").listen();
+statsGui.add(treeStats,"branchPointsRemoved").listen();
+
 
 
 var scene = new THREE.Scene();
@@ -174,12 +191,16 @@ class Tree extends THREE.Object3D {
     constructor() {
         super();
 
+        this.debugObject = new THREE.Object3D();
+        this.add(this.debugObject);
+
         this.config = Object.assign({
             trunkHeight: 3,
             attractionPointRadius: 1.8,
             attractionPointCount:40,
             attractionPointHeightOffset: -1,
             attractionVerticalRatio: 1,
+            attractionPointArc: Math.PI,
             drawAttractionPoints: false,
             trunkRadialSegments: 5,
             trunkBaseRadius: 0.2,
@@ -187,9 +208,11 @@ class Tree extends THREE.Object3D {
             branchSteps: 7,
             branchStepSize: 0.3,
             branchRadialSegments: 5,
+            branchSimplifyEpsilon: 0.2,
             leafDepthRatio: 0.2,
             drawFoliage: true,
             drawBranchPoints: false,
+            drawBranchFullPath: false,
             drawTrunk: true
         }, params);
 
@@ -211,14 +234,14 @@ class Tree extends THREE.Object3D {
         this.aps = [];
         for (var i=0;i<this.config.attractionPointCount;i++) {
             var azimuth = Math.random()*Math.PI*2;
-            var elevation = Math.random()*Math.PI//-1.5;
+            var elevation = Math.PI/2 + Math.random()*this.config.attractionPointArc;
 
             var x = this.config.attractionPointRadius * Math.cos(elevation) * Math.cos(azimuth);
             var z = this.config.attractionPointRadius * Math.cos(elevation) * Math.sin(azimuth);
             var y = this.config.trunkHeight + this.config.attractionPointHeightOffset + this.config.attractionPointRadius * Math.sin(elevation) * this.config.attractionVerticalRatio;
             this.aps.push({x:x,y:y,z:z,dToNearest:1000});
             if (this.config.drawAttractionPoints) {
-                this.add(markPoint(x,y,z,0xff9900));
+                this.debugObject.add(markPoint(x,y,z,0xff9900));
             }
         }
 
@@ -287,6 +310,10 @@ class Tree extends THREE.Object3D {
             this.iterate(this.stage-1);
         } else if (this.stage === this.config.branchSteps) {
             this.simplify()
+            if (this.config.drawBranchFullPath) {
+                this.drawBranches(false);
+            }
+            this.drawBranches(true);
         } else {
             if (this.config.drawFoliage) {
                 this.leafNodes.forEach(function(p) {
@@ -334,7 +361,6 @@ class Tree extends THREE.Object3D {
         })
         newPoints.forEach(function(np) {
             self.points.push(np.to);
-            // self.add(markPoint(np.to.x,np.to.y,np.to.z,c));
             self.calculateDistancesForPointAt(np.to);
             var line = markLine(np.from,np.to,0x996633,self.config.branchRadius,self.config.branchRadialSegments);
             self.workingBranches.push(line);
@@ -368,7 +394,7 @@ class Tree extends THREE.Object3D {
 
         if (this.config.drawBranchPoints) {
             this.points.forEach(function(p) {
-                self.add(markPoint(p.x,p.y,p.z,0x0000ff,0.1));
+                self.debugObject.add(markPoint(p.x,p.y,p.z,0x0000ff,0.1));
             });
         }
         var branches = [];
@@ -400,9 +426,49 @@ class Tree extends THREE.Object3D {
                 }
             }
         }
+
+
+        this.branches = branches;
+        this.simpleBranches = [];
+        this.branchPointsRemoved = 0;
+        this.branches.forEach(function(b) {
+            if (b.length > 2) {
+                var newBranch = simplifyBranch(b,self.config.branchSimplifyEpsilon)
+                self.simpleBranches.push(newBranch);
+                self.branchPointsRemoved += (b.length - newBranch.length)
+            } else {
+                self.simpleBranches.push(b);
+            }
+        })
+    }
+
+    drawBranches(simple) {
+        var self = this;
+        //
+        // this.workingBranches.forEach(function(b) {
+        //     self.remove(b);
+        // })
+
+        var branches = this.branches;
+        if (simple) {
+            branches = this.simpleBranches;
+        }
+
         branches.forEach(function(b) {
             if (b.length > 1) {
-                self.add(markPath(b,0x996633,self.config.branchRadius,self.config.branchRadialSegments));
+                var path = markPath(b,simple?0x996633:0xffff00,simple?self.config.branchRadius:0.03,self.config.branchRadialSegments);
+                self.workingBranches.push(path);
+                if (simple) {
+                    self.add(path);
+                } else {
+                    self.debugObject.add(path);
+                }
+
+                if (self.config.drawBranchPoints) {
+                    b.forEach(function(p) {
+                        self.debugObject.add(markPoint(p.x,p.y,p.z,0xff0000,0.1));
+                    });
+                }
 
             }
         })
@@ -426,19 +492,89 @@ class Tree extends THREE.Object3D {
         })
     }
 
+    stats() {
+        var self = this;
+        var vc = 0;
+        this.children.forEach(function(g) { if (g !== self.debugObject) vc += g.geometry.vertices.length;})
+        return {
+            vertexCount: vc,
+            branchPointsRemoved: this.branchPointsRemoved
+        }
+    }
+
 }
 
-function getBranches(p) {
-
-    if (p.connected.length === 0) {
-        return p;
-    } else {
-        var result = [p];
-        p.connected.forEach(function(p2) {
-            result.push([getChildBranches(p2)])
-        })
-        return result;
+function simplifyBranch(points, epsilon) {
+    var subBranches = [];
+    var currentBranch = [];
+    for (var i=0;i<points.length;i++) {
+        currentBranch.push(points[i]);
+        if (points[i].connected.length > 1 || i === points.length-1) {
+            subBranches.push(currentBranch);
+            currentBranch = [points[i]];
+        }
     }
+    var result = [];
+    subBranches.forEach(function(sb) {
+        var simplifiedSubBranch = simplifyLine(sb,epsilon);
+        if (result.length === 0) {
+            result = simplifiedSubBranch;
+        } else {
+            result = result.concat(simplifiedSubBranch.slice(1));
+        }
+    })
+    return result;
+
+
+}
+function simplifyLine(points, epsilon) {
+    if (points.length < 3) {
+        return points;
+    }
+    // Find the point with the maximum distance
+    var dmax = 0;
+    var index = 0;
+    var end = points.length
+    for (var i=1; i<end-1; i++) {
+        var d = getPointToLineDistance(points[i], points[0], points[end-1])
+        if (d > dmax) {
+            index = i
+            dmax = d
+        }
+    }
+    var result = [];
+
+    // If max distance is greater than epsilon, recursively simplify
+    if (dmax > epsilon) {
+        // Recursive call
+        var r1 = simplifyLine(points.slice(0,index+1), epsilon)
+        var r2 = simplifyLine(points.slice(index+1), epsilon)
+        result = r1.slice(0,-1).concat(r2)
+    } else {
+        result = [points[0],points[end-1]]
+    }
+    return result;
+}
+var p0 = new THREE.Vector3();
+var p1 = new THREE.Vector3();
+var p2 = new THREE.Vector3();
+
+var d1 = new THREE.Vector3();
+var d2 = new THREE.Vector3();
+var d3 = new THREE.Vector3();
+
+function getPointToLineDistance(p,l0,l1) {
+    // |(p0-p1)x(p0-p2)| / |p2-p1]
+    p0.set(p.x,p.y,p.z);
+    p1.set(l0.x,l0.y,l0.z);
+    p2.set(l1.x,l1.y,l1.z);
+
+    d1.subVectors(p0,p1);
+    d2.subVectors(p0,p2);
+    var s1 = d1.cross(d2).length();
+    var s2 = d3.subVectors(p2,p1).length();
+    return s1/s2;
+    // var d1 =
 }
 
 var generating = false;
@@ -449,6 +585,11 @@ function generateTree() {
     if (currentTree) {
         scene.remove(currentTree);
     }
+    Object.keys(treeStats).forEach(function(s) {
+        treeStats[s] = "";
+    })
+
+
     currentTree = new Tree();
     scene.add(currentTree);
     stepTree();
@@ -467,9 +608,12 @@ function stepTree() {
                 }
             },3000)
         }
-        var vc = 0;
-        currentTree.children.forEach(function(g) { vc += g.geometry.vertices.length;})
-        console.log("Vertex count",vc);
+        var stats = currentTree.stats();
+        Object.keys(stats).forEach(function(s) {
+            if (treeStats.hasOwnProperty(s)) {
+                treeStats[s] = stats[s];
+            }
+        })
     }
 }
 generateTree();
